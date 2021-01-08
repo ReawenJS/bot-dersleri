adc6fcd4743812c511b515f8e62ae8a9197ffe09
HeliosJS <75804397+ReawenJS@users.noreply.github.com>
2021-01-08 19:38:59 +0300

    Create TagAlanaRolVerme.js

---
+++ OtoTag/TagAlanaRolVerme.js
@@ -0,0 +1,64 @@
+              client.on("userUpdate", async (oldUser, newUser) => { //reawen
+                let sunucu = `sunuuc id`;//reawen
+                let kanal = `kanal id`;//reawen
+                let taglı = `taglı rol id`;//reawen
+              //reawen
+                let tag = `taglı sembol`;//reawen
+                let untag = `tagsız sembol`;//reawen
+              //reawen
+                let channel = client.guilds.cache.get(sunucu).channels.cache.get(kanal);//reawen
+              //reawen
+                if (oldUser.username !== newUser.username) {//reawen
+                  if (//reawen
+                    newUser.username.includes(tag) &&//reawen
+                    !client.guilds.cache//reawen
+                      .get(sunucu)//reawen
+                      .members.cache.get(newUser.id)//reawen
+                      .roles.cache.has(taglı)//reawen
+                  ) {//reawen
+                    await client.guilds.cache//reawen
+                      .get(sunucu)//reawen
+                      .members.cache.get(newUser.id)//reawen
+                      .roles.add(taglı);//reawen
+              //reawen
+                    await client.guilds.cache//reawen
+                      .get(sunucu)//reawen
+                      .members.cache.get(newUser.id)//reawen
+                      .setNickname(//reawen
+                        client.guilds.cache//reawen
+                          .get(sunucu)//reawen
+                          .members.cache.get(newUser.id)//reawen
+                          .displayName.replace(untag, tag)//reawen
+                      );//reawen
+              //reawen
+               //reawen
+                    channel.send(`${newUser} adlı kullanıcı "${tag}" sembolünü kullanıcı adına ekleyerek ailemize katıldı.`);//reawen
+                  }//reawen
+                  if (//reawen
+                    !newUser.username.includes(tag) &&//reawen
+                    client.guilds.cache//reawen
+                      .get(sunucu)//reawen
+                      .members.cache.get(newUser.id)//reawen
+                      .roles.cache.has(taglı)//reawen
+                  ) {//reawen
+                    await client.guilds.cache//reawen
+                      .get(sunucu)//reawen
+                      .members.cache.get(newUser.id)//reawen
+                      .roles.remove(taglı);//reawen
+              //reawen
+                    await client.guilds.cache//reawen
+                      .get(sunucu)//reawen
+                      .members.cache.get(newUser.id)//reawen
+                      .setNickname(//reawen
+                        client.guilds.cache//reawen
+                          .get(sunucu)//reawen
+                          .members.cache.get(newUser.id)//reawen
+                          .displayName.replace(tag, untag)//reawen
+                      );//reawen
+                    channel.send(`${newUser} adlı kullanıcı "${tag}" sembolünü kullanıcı adından kaldırarak ailemizden ayrıldı.`);//reawen
+                  }//reawen
+                }//reawen
+              });//reawen
+//reawen
+
+> 0'dan yaptım alın kullanın. Açıklaması yok id yazın kullanın
